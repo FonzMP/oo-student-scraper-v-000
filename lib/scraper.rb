2cc13a644a451a915d4667dad6a6284e9a136706
Alex Wilson <fonzmp@gmail.com>
2018-05-25 23:39:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,6 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html_doc = Nokogiri::HTML("../fixtures/student-site/index.html")
-    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
