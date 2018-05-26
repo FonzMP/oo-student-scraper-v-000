8e94056043c1fff1313df99cc4d1691a4b229e0b
Alex Wilson <fonzmp@gmail.com>
2018-05-26 04:37:59 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -49,6 +49,7 @@ class Scraper
     bio = doc.css("div.description-holder p").text
 
     student_hash = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog, :profile_quote => profile_quote, :bio => bio}
+    student_hash.delete("")
   end
 
 end
