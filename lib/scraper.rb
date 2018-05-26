a591a8a4393e058a34482fe5b2f376eddfe36a44
Alex Wilson <fonzmp@gmail.com>
2018-05-26 04:03:32 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -39,6 +39,35 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open("#{profile_url}"))
+
+    holder = []
+    holder_two = []
+    holder_three = []
+    students = []
+
+    # student name
+    doc.css(".student-name").collect do |item|
+      holder << item.text
+    end
+
+    #student location
+    doc.css(".student-location").collect do |item|
+      holder_two << item.text
+    end
+
+    #student URL
+    doc.css("div.student-card a").collect do |item|
+      holder_three << item['href']
+    end
+
+    #create hash and push to array for return
+    counter = 0
+    while counter < holder.length
+      students << {:name => holder[counter], :location => holder_two[counter], :profile_url => holder_three[counter]}
+      counter += 1
+    end
+
+    students
   end
 
 end
