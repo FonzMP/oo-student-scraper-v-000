8518cc7830b05e86e5c6a19ca34a3d7c72ccf5c2
Alex Wilson <fonzmp@gmail.com>
2018-05-26 03:05:48 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
     doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
     holder = []
     doc.css(".student-name").collect do |item|
-      {:name => item.text}
+      holder << {:name => item.text}
     end
     binding.pry
   end
