dee15852ca82f4c91d494ad7309cffff9efe27f5
Alex Wilson <fonzmp@gmail.com>
2018-05-26 04:43:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -50,7 +50,7 @@ class Scraper
 
     student_hash = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog, :profile_quote => profile_quote, :bio => bio}
     clean_hash = student_hash.delete_if {|k, v| v == nil}
-
+    clean_hash
   end
 
 end
