449a7a71092c22d421ec2f2db0150ee7debc6702
Alex Wilson <fonzmp@gmail.com>
2018-05-26 03:02:01 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("./fixtures/student-site/index.html"))
     name = doc.css("h4 .student-name")
+    binding.pry
   end
 
 
