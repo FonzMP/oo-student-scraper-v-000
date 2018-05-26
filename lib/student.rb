9156508741653f6cec3cdde3da42f46729d75bf6
Alex Wilson <fonzmp@gmail.com>
2018-05-26 17:43:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,12 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    binding.pry
+    self.twitter = attributes_hash[:twitter]
+    self.linkedin = attributes_hash[:linkedin]
+    self.twitter = attributes_hash[:twitter]
+    self.twitter = attributes_hash[:twitter]
+    self.twitter = attributes_hash[:twitter]
+    self.twitter = attributes_hash[:twitter]
   end
 
   def self.all
