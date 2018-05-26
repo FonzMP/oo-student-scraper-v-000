bd6c3eef84faaac42516567601d2eb107964e7af
Alex Wilson <fonzmp@gmail.com>
2018-05-26 16:57:38 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -54,7 +54,9 @@ class Scraper
       links_holder << link['href']
     end
 
-    binding.pry
+    links_holder.each do |assign|
+      if assign.include?("twitter")
+        twitter = assign
 
     student_hash = {:twitter => twitter, :linkedin => linkedin, :github => github, :blog => blog, :profile_quote => profile_quote, :bio => bio}
     clean_hash = student_hash.delete_if {|k, v| v == nil}
